f78306d2f01bb05884e7690afaf9d2b2b8d98373
nomiverse <lukas@nomiverse.com>
2026-05-26 10:00:27 +0200

    Add outputFileTracingIncludes for deck.html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    '/api/deck': ['./deck.html'],
+  },
 }
 
 module.exports = nextConfig
